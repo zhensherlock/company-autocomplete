b8882f2463d8cc1a0335bab1c56dc4276548c8f1
MichaelSun <zhensherlock@126.com>
2024-09-18 11:11:02 +0800

    feat: update api

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,12 +1,12 @@
 import type { CompanyAutocompleteOptions } from '../../types'
-import { queryQccOpenAPI } from './qcc-open'
+// import { queryQccOpenAPI } from './qcc-open'
 import { queryClearBitAPI } from './clear-bit'
-import { queryQccGlobalAPI } from './qcc-global'
+// import { queryQccGlobalAPI } from './qcc-global'
 
 export const handleQueryData = async (keyword: string, options: CompanyAutocompleteOptions) => {
   switch (options.api) {
-    case 'qcc_open':
-      return await queryQccOpenAPI(keyword)
+    // case 'qcc_open':
+    //   return await queryQccOpenAPI(keyword)
     // case 'qcc_global':
     //   return await queryQccGlobalAPI(keyword)
     case 'clearbit':
